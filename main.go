a56f370a5442b21f1a8ccc2f1b22e89b713265ca
Pavel Belokon <pbelokon@myseneca.ca>
2024-08-30 16:24:08 -0400

    added handler

--- main.go
+++ main.go
@@ -5,12 +5,15 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) { 
-		w.Write([]byte("Hello!"))
-	})
-	err := http.ListenAndServe(":8888", nil)
+func handleHello(w http.ResponseWriter, r *http.Request) { 
+	w.Write([]byte("Hello!"))
+}
 
+func main() {
+	server := http.NewServeMux()
+	server.HandleFunc("/hello", handleHello)
+
+	err := http.ListenAndServe(":8888", server)
 	if err != nil { 
 		fmt.Println(("Error while opening the server"))
 	}
