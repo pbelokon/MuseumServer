91e6c81c0f91ae150d91d59d5b398dc4c919ec7e
Pavel Belokon <pbelokon@myseneca.ca>
2024-08-31 00:16:39 -0400

    tamplate html

--- main.go
+++ main.go
@@ -13,6 +13,10 @@ func main() {
 	server := http.NewServeMux()
 	server.HandleFunc("/hello", handleHello)
 
+
+	fs := http.FileServer(http.Dir("./public"))
+	server.Handle("/", fs)
+
 	err := http.ListenAndServe(":8888", server)
 	if err != nil { 
 		fmt.Println(("Error while opening the server"))
